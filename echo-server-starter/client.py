576339c008e09325689116438861314e351cd358
Miles <mblack@platformq.com>
2018-12-11 22:37:16 -0500

    Fixes with the client.

--- echo-server-starter/client.py
+++ echo-server-starter/client.py
@@ -3,7 +3,7 @@ from threading import Thread
 import threading
 import sys
 
-SERVER_NAME = 'localhost'
+SERVER_NAME = "localhost"
 SERVER_PORT = 3000
 
 
@@ -12,95 +12,124 @@ def connect():
     sock = socket(AF_INET, SOCK_STREAM)
     # Client socket connection to the server
     sock.connect((SERVER_NAME, SERVER_PORT))
+    sock.setblocking(False)
 
     return sock
 
 
 def send(sock, message):
-    sock.send(bytearray(message, 'utf-8'))
+    sock.send(bytearray(message, "utf-8"))
 
 
 def recv(sock):
-    return sock.recv(1024).decode('utf-8')
+    try:
+        msg = sock.recv(1024).decode("utf-8")
+    except:
+        msg = ""
+
+    return msg
 
 
 def list(sock):
-    send(sock, '-')
+    send(sock, "-")
     result = recv(sock).rstrip()
-    if result == '':
+    if result == "":
         return []
     else:
-        return result.split(',')[0:-1]
+        return result.split(",")[0:-1]
 
 
 def last_list(sock):
     xs = list(sock)
+    print(xs)
     if len(xs) == 0:
-        return ''
+        return ""
     else:
         return xs[-1]
 
 
-def ask(prompt=':-p'):
+def ask(prompt=":-p"):
     return input(prompt)
 
 
 def prompt_on_last(sock):
     last = last_list(sock)
-    if last == '':
+    if last == "":
         return ask()
     else:
         return ask(last)
 
+
 def fileclient(f):
-    #lock = threading.Lock()
     fd = open(f, "r")
     connection = connect()
-    thread = Thread(target = message_listener, args = (connection))
+    thread = Thread(target=message_listener, args=(connection,))
     thread.start()
-    send_command_file(connection, fd)
     thread.join()
+    thread = Thread(target=send_command_file, args=(connection, fd))
+    thread.start()
+    thread.join()
+
+
+"""
+# Old client method (BROKEN)
+def client():
+    connection = connect()
+    thread = Thread(target=message_listener, args=(connection,))
+    thread.start()
+    thread.join()
+    thread = Thread(target=send_command, args=(connection,))
+    thread.start()
+    thread.join()
+"""
+
 
 def client():
-    #lock = threading.Lock()
+    lock = threading.Lock()
+    # connect to the server, connection is a socket here
     connection = connect()
-    thread = Thread(target = message_listener, args = (connection))
+    # create the message_listener thread, and then start it up
+    thread = Thread(target=message_listener, args=(connection, lock))
     thread.start()
-    send_command(connection)
+    # send_command will loop to send and prompt for messages
+    send_commands(connection, lock)
+    # by this point, the client will have entered "quit", so at
+    # this point we can join the message_listener thread and program will end:
     thread.join()
 
-def message_listener(connection):
+
+def message_listener(connection, lock):
+    # lock = threading.Lock()
     while 1:
-        #lock.acquire()
+        # lock.acquire()
         response = recv(connection)
+        # lock.release()
         if len(response) > 0:
             print(response.strip() + "\n")
-        #lock.release()
+
 
 def send_command_file(connection, fd):
     sentence = fd.readline()
-	#opens passed in file, reads and sends messages as if the user was inputting commands.
-    while sentence != '':
-        #lock.acquire()
+    # opens passed in file, reads and sends messages as if the user was inputting commands.
+    while sentence != "":
         print(sentence)
         send(connection, sentence)
-        #response = recv(connection)
-        #print(response.strip())
+        response = recv(connection)
+        print(response.strip())
         sentence = fd.readline()
-        #lock.release()
 
-def send_command(connection):
+
+def send_commands(connection, lock):
     sentence = prompt_on_last(connection)
-    while sentence != 'quit':
-        #lock.acquire()
+    while sentence != "quit":
+        # lock.acquire()
         send(connection, sentence)
-        #response = recv(connection)
-        #print(response.strip())
         sentence = prompt_on_last(connection)
-        #lock.release()
+        # lock.release()
 
-if __name__=="__main__":
-	if len(sys.argv) == 1 :#checks for case where a file is passed in
-		client()
-	elif len(sys.argv) > 1:#if there is a file provided, let's load commands.
-		fileclient(sys.argv[1])
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:  # checks for case where a file is passed in
+        client()
+    elif len(sys.argv) > 1:  # if there is a file provided, let's load commands.
+        fileclient(sys.argv[1])
